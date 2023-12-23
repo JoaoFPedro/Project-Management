2fa8c400e249ce0ed60edd47c646965a16242461
Joao Pedro <joao.fpsilva98@gmail.com>
2023-12-23 13:08:03 -0300

    Starting project - SIdeBarMenu

---
+++ src/components/ProjectsSideBar.jsx
@@ -0,0 +1,14 @@
+function ProjectsSidebar (){
+    return( 
+    <aside>
+        <h2>Your Projects</h2>
+        <div>
+            <button>
+                + Add Project
+            </button>
+        </div>
+    </aside>
+    )
+}
+
+export default  ProjectsSidebar;
