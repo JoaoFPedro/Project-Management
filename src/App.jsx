062f69db1e5885ddba577da6618a98e000cbc932
Joao Pedro <joao.fpsilva98@gmail.com>
2023-12-29 20:02:21 -0300

    Creat Button, Input components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import ProjectsSidebar from "./components/ProjectsSideBar";
 import NewProject from "./components/NewProject";
+import  NoProjectSelected from "./components/NoProjectSelected";
 function App() {
   return (
    <main className="h-screen my-8 flex gap-8">
    <ProjectsSidebar />
-   <NewProject />
+   
+   <NoProjectSelected/>
+   
    </main>
   );
 }
