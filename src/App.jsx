d267a560ceae3c2acd21bfce2957d2bb824e6683
Joao Pedro <joao.fpsilva98@gmail.com>
2023-12-29 21:50:52 -0300

    creating function to deal with the change components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,29 @@
 import ProjectsSidebar from "./components/ProjectsSideBar";
 import NewProject from "./components/NewProject";
 import  NoProjectSelected from "./components/NoProjectSelected";
+import { useState } from "react";
+
+
 function App() {
+const [projectState, setProjectState] =   useState({
+  selecte4dProjectId: undefined,
+  projects: []
+
+})
+
+function handleStartAddProject(){
+  setProjectState(prevState =>{
+    return {
+      ...prevState,
+      selecte4dProjectId: null,
+    };
+  })
+}
   return (
    <main className="h-screen my-8 flex gap-8">
    <ProjectsSidebar />
    
-   <NoProjectSelected/>
+   <NoProjectSelected onStartedAddProject={handleStartAddProject}/>
    
    </main>
   );
--- src/components/NoProjectSelected.jsx
+++ src/components/NoProjectSelected.jsx
@@ -4,7 +4,7 @@ import Button from './Button.jsx'
 
  function NoProjectSelected(){
     return(
-        <div>
+        <div className='mt-24 text-center w-2/3'>
             <img src={noProjectImage} alt='An empty task list' className='w-16 h-16 object-contain mx-auto'/>
             <h2 className='text-xl font-bold text-stone-500 my-4'>
                 No Project Selected
