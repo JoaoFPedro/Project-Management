194044f78abbc054eb5c517f5c1840fedd3a5275
Joao Pedro <joao.fpsilva98@gmail.com>
2024-01-15 19:53:02 -0300

    handling deletaion button

--- src/App.jsx
+++ src/App.jsx
@@ -1,61 +1,89 @@
 import ProjectsSidebar from "./components/ProjectsSideBar.jsx";
 import NewProject from "./components/NewProject.jsx";
-import  NoProjectSelected from "./components/NoProjectSelected.jsx";
+import NoProjectSelected from "./components/NoProjectSelected.jsx";
 import { useState } from "react";
-
+import SelectedProject from "./components/SelectedProject.jsx";
 
 function App() {
-const [projectState, setProjectState] =   useState({
-  selecte4dProjectId: undefined,
-  projects: []
-
-})
-
-function handleStartAddProject(){
-  setProjectState(prevState =>{
-    return {
-      ...prevState,
-      selecte4dProjectId: null,
-    };
+  const [projectState, setProjectState] = useState({
+    selectedProjectId: undefined,
+    projects: [],
   });
-}
 
-function handleCancelProject(){
-  setProjectState(prevState =>{
-    return {
-      ...prevState,
-      selecte4dProjectId: undefined,
-    };
-  });
-}
 
-function handleAddProject(projecData){
-  setProjectState(prevState => {
-    const newProject = {
-      ...projecData,
-         id:Math.random(),
-    };
-    return {
-      ...prevState,
-      selecte4dProjectId: undefined,
-      projects:[...prevState.projects, newProject]
-    }
-  })
-  
-}
-console.log(projectState);
-let content;
-if(projectState.selecte4dProjectId === null){
-  content = <NewProject onAdd={handleAddProject} onCancel={handleCancelProject} />;
-}else if(projectState.selecte4dProjectId === undefined){
-  content = <NoProjectSelected onStartAddProject={handleStartAddProject}/>;
-   
-}
-  return (    
-   <main className="h-screen my-8 flex gap-8">
-   <ProjectsSidebar onStartAddProject={handleStartAddProject} projects ={projectState.projects}/>   
-   {content}   
-   </main>
+  function handleSelectedProject(id) {
+    setProjectState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: id,
+      };
+    });
+  }
+
+  function handleDeleteProject() {
+    setProjectState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+        projects: prevState.projects.filter(
+          (project) => project.id !== prevState.selectedProjectId
+        ),
+      };
+    });
+  }
+
+  function handleStartAddProject() {
+    setProjectState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      };
+    });
+  }
+
+  function handleCancelProject() {
+    setProjectState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+      };
+    });
+  }
+
+  function handleAddProject(projecData) {
+    setProjectState((prevState) => {
+      const newProject = {
+        ...projecData,
+        id: Math.random(),
+      };
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+        projects: [...prevState.projects, newProject],
+      };
+    });
+  }
+  const selectedProject = projectState.projects.find(
+    (project) => project.id === projectState.selectedProjectId
+  );
+
+  let content = <SelectedProject project={selectedProject} onDelete={handleDeleteProject}/>;
+  if (projectState.selectedProjectId === null) {
+    content = (
+      <NewProject onAdd={handleAddProject} onCancel={handleCancelProject} />
+    );
+  } else if (projectState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
+  }
+  return (
+    <main className="h-screen my-8 flex gap-8">
+      <ProjectsSidebar
+        onStartAddProject={handleStartAddProject}
+        projects={projectState.projects}
+        onSelectedProject={handleSelectedProject}
+      />
+      {content}
+    </main>
   );
 }
 
---
+++ src/components/SelectedProject.jsx
@@ -0,0 +1,32 @@
+function SelectedProject({ project, onDelete }) {
+  const formattedDate = new Date(project.dueDate).toLocaleDateString("pt-BR", {
+    year: "numeric",
+    month: "short",
+    day: "2-digit",
+  });
+
+  return (
+    <div className="w-[35rem] mt-16">
+      <header className="pb-4 mb-4 border-b-2 border-stone-300">
+        <div className="flex items-center justify-between">
+          <h1 className="text-3xl font-bold text-stone-600 mb-2">
+            {project.title}
+          </h1>
+          <button
+            className="px-6 py-2 text-white bg-red-700"
+            onClick={onDelete}
+          >
+            Delete
+          </button>
+        </div>
+        <p className="mb-4 text-stone-400">{formattedDate}</p>
+        <p className="text-stone-600 whitespace-pre-wrap">
+          {project.description}
+        </p>
+      </header>
+      TASKS
+    </div>
+  );
+}
+
+export default SelectedProject;
